f3819b839d784d5041ac185d3e21908aa7ad281a
talipa <talipayan6035@gmail.com>
2023-08-16 15:43:22 +0200

    Tagler bitmek uzere

--- smartedu_con/courses/urls.py
+++ smartedu_con/courses/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     
     path('', views.course_list, name="courses"),
     path('<slug:category_slug>/<int:course_id>', views.course_details, name="course_detail"),
+    path('categories/<slug:category_slug>', views.category_details, name="courses_by_category"),
 
 ]
