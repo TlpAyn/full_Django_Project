d7b11337a7b5646dc6baea5426f88ffd2dc40357
talipa <talipayan6035@gmail.com>
2023-08-17 14:57:29 +0200

    urleler zaten tamamlandi

--- smartedu_con/courses/urls.py
+++ smartedu_con/courses/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     
     path('', views.course_list, name="courses"),
     path('<slug:category_slug>/<int:course_id>', views.course_details, name="course_detail"),
-    path('categories/<slug:category_slug>', views.category_details, name="courses_by_category"),
+    path('categories/<slug:category_slug>', views.category_list, name="courses_by_category"),
+   # path('tags/<slug:category_slug>', views.tag_list, name="courses_by_tag"),
 
 ]
