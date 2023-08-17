72983a3aaf3906f83f6a405a7352bbc69b1bf794
talipa <talipayan6035@gmail.com>
2023-08-17 14:57:41 +0200

    bunu da gec

--- smartedu_con/pages/urls.py
+++ smartedu_con/pages/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from . import views
+#from . import views
+from pages.views import AboutView, IndexView
 
 urlpatterns = [
     
-    path('', views.index, name="index"),
-    path('', views.index),
-    path('about', views.about, name="about"),
+    #path('', views.index, name="index"),
+    path('about/', AboutView.as_view(), name="about"),
+    path('', IndexView.as_view(), name="index"),
+  #  path('', views.index),
+    #path('about', views.about, name="about"),
+
 ]
+ 
