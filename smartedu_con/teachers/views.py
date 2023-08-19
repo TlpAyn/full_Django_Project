d66f16e43a53db7bceef6e4355682eaa89c22df6
talipa <talipayan6035@gmail.com>
2023-08-19 23:50:28 +0200

    url and vs

--- smartedu_con/teachers/views.py
+++ smartedu_con/teachers/views.py
@@ -1,5 +1,8 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from teachers.models import Teacher
 
 
@@ -9,3 +12,16 @@ class  TeacherListView(ListView):
     model = Teacher
     template_name = 'teachers.html' 
     context_object_name = 'teachers'
+    queryset = Teacher.objects.all()[:3]
+    #paginate_by = 1
+
+
+    #def get_queryset(self):
+        #return Teacher.objects.all()[:3]
+
+class TeacherDetailView(DetailView):
+    model = Teacher
+    tepmlate_name = 'teacher.html'
+
+
+
